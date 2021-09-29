f673a8db716224b9cf1a521019b955d641693a48
joseluiz123 <joseluiz140697@gmail.com>
2021-09-29 13:50:34 -0300

    del

--- 2- filiais.sql
+++
@@ -1,9 +0,0 @@
-create table filial (
-  cod_filial int NOT NULL PRIMARY KEY,
-  estado varchar(2),
-  endereco varchar(50),
-  cep int
-);
-
-insert into filial values (1, 'RJ', 'Avenida Almirante Barroso 91', 20031916);
-insert into filial values (2, 'SP', 'Praça Antônio Prado, 33', 01010900);
